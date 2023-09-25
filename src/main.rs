282acac502f9dfbf32c909ae8b6dd61e7173b16c
Space Nerd <florianpospiech77@gmail.com>
2023-09-25 21:20:24 +0200

    Well now it works but it doesnt at the same time....

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,40 @@
 use std::cmp::Ordering;
 use std::fmt::Debug;
+
 #[derive(Debug)]
-pub struct Node<T>
-    where
-        T: Ord + Debug + Copy
+pub struct Node<T: Ord>
 {
-    key: Option<T>,
+    key: T,
     left: Option<Box<Node<T>>>,
-    right: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>
 }
 
-pub fn new() -> Node<T> {
-    Node {
-        key: None,
-        left: None,
-        right: None,
+impl<T: Ord> Node<T> {
+    pub fn new(key: T) -> Node<T> {
+        Node {
+            key,
+            left: None,
+            right: None,
+        }
     }
-}
 
-pub fn from(key: T) -> Node<T> {
-    Node {
-        key: Some(key),
-        left: None,
-        right: None,
-    }
-}
-
-pub fn insert(&mut self, key: T) {
-    match &self.key {
-        Some(key) => {
-            let target = match key.cmp(&key) {
-                Ordering::Less => &mut self.left,
-                Ordering::Equal => &mut self.left,
-                Ordering::Greater => &mut self.right,
-            };
-            match target {
-                Some(ref mut node) => {
-                    node.insert(key)
-                }
-                None => {
-                    let node: Node<T> = Node::from(key);
-                    *target = Some(Box::new(node))
-                }
+    pub fn insert(mut root: &mut Option<Box<Node<T>>>, key: T) {
+        while let Some(ref mut node) = root{
+            match key.cmp(&node.key) {
+                Ordering::Equal => root = &mut node.left,
+                Ordering::Less => root = &mut node.left,
+                Ordering::Greater => root = &mut node.right,
             }
         }
-        None => self.key = Some(key),
+        *root = Some(Box::new(Node::new(key)))
     }
-}
 
-pub fn minimum(&self) -> Option<&T> {
-    match &self.left {
-        Some(node) => node.minimum(),
-        None => match &self.key {
-            Some(key) => Some(&key),
-            None => None,
-        },
-    }
-}
-
-pub fn maximum(&self) -> Option<&T> {
-    match &self.right {
-        Some(node) => node.maximum(),
-        None => match &self.key {
-            Some(key) => Some(&key),
-            None => None,
-        },
-    }
-}
-
-pub fn search(&self, target: &T) -> bool {
-    match &self.key {
-        Some(key) => match &target.cmp(key) {
-            Ordering::Equal => return true,
-            Ordering::Less => match &self.left {
-                Some(node) => node.search(target),
-                None => false,
-            },
-            Ordering::Greater => match &self.right {
-                Some(node) => node.search(target),
-                None => false,
-            },
-        },
-        None => return false,
-    }
 }
 
 fn main() {
-    let node1 = new();
-    let node2 = new();
+    let mut node = Node::new(10);
+    println!("{:?}", node);
 
-    println!("{:?}", node1);
-    println!("{:?}", node2);
+
+    println!("{:?}", Node::insert(&mut Some(Box::new(node)), 20));
 }
